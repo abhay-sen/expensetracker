a2fdf3568dfde07d303a9e855d076a0207993d1e
abhay-sen <abhaysen0502@gmail.com>
2024-03-20 23:39:59 +0530

    commit

--- expense-tracker/src/components/pages/SignInPage.jsx
+++ expense-tracker/src/components/pages/SignInPage.jsx
@@ -3,7 +3,7 @@ import './../styles/SignInPage.css';
 function SignInPage(){
     return <div>
         <div className="block">
-            <img src="https://drive.google.com/file/d/1oq1GYNdGFDe43eEFIc68KNK5RcFFSoog/view?usp=sharing" alt="logo"/>
+            <img src="https://github.com/abhay-sen/expensetracker/blob/main/expense-tracker/src/assets/imgs/Screenshot_2024-03-20_163420-removebg.jpeg?raw=true" alt="logo"/>
             <div className="contents">
                 <label htmlFor="username" >Username:</label>
                 <input type="text" id='userName' name='userName'placeholder='Enter Username'/>
