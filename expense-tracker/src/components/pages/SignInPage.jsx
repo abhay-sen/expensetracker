cb9572cd5eda5781b95f20c93eafe6ded42da7dc
abhay-sen <abhaysen0502@gmail.com>
2024-03-20 17:40:26 +0530

    hmm

--- expense-tracker/src/components/pages/SignInPage.jsx
+++ expense-tracker/src/components/pages/SignInPage.jsx
@@ -3,8 +3,15 @@ import './../styles/SignInPage.css';
 function SignInPage(){
     return <div>
         <div className="block">
-            <label htmlFor="username" >Username:</label><br />
-            <input type="text" id='userName' name='userName'placeholder='Enter Username'/><br />
+            <img src="https://drive.google.com/file/d/1oq1GYNdGFDe43eEFIc68KNK5RcFFSoog/view?usp=sharing" alt="logo"/>
+            <div className="contents">
+                <label htmlFor="username" >Username:</label>
+                <input type="text" id='userName' name='userName'placeholder='Enter Username'/>
+                <label htmlFor="password">Password:</label>
+                <input type="password" id='userName' name='passWord' placeholder='Enter Password'/>
+                <button type='submit'>Login</button>
+            </div>
+            
         </div>
     </div>;
 }
